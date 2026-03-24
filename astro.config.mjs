9f2005fcd543ca2932084d61e644167811686d35
a5ucanc <a5ucanc@users.noreply.github.com>
2026-03-24 22:48:36 +0200

    fix: force vite to pre-bundle fuse.js at startup to fix dev MIME type error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://a5ucanc.github.io',
+  vite: {
+    optimizeDeps: {
+      include: ['fuse.js'],
+    },
+  },
 });
